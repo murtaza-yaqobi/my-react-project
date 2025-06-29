e8be25c260f7d089a077f14cdf58ef9b0b025c56
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-06-29 05:43:01 +0430

    K

--- src/componets/Text.jsx
+++ src/componets/Text.jsx
@@ -2,6 +2,7 @@ function Text(){
     return (
         <div>
             <h1 className="text-4xl font-bold ml-40">مهم نیست کوه چقدر بزرگ باشه خورشید همیشه طلوع می کنه</h1>  
+            <h1 className="text-4xl font-bold ml-40">مهم نیست کوه چقدر بزرگ باشه خورشید همیشه طلوع می کنه</h1>  
         </div>
     )
 }
